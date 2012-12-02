08ed74efbec1fe0b056c29f034e53943ac6b5165
Jamie Wright <jamie@brilliantfantastic.com>
2012-12-02 12:29:36 -0500

    Default configuration values

--- lib/hoarder/passbook/configuration.rb
+++ lib/hoarder/passbook/configuration.rb
@@ -1,7 +1,19 @@
 module Hoarder
   module Passbook
     class Configuration
-      attr_accessor :base_path
+      attr_accessor :base_path,
+                    :logo,
+                    :logo_2x,
+                    :icon,
+                    :icon_2x
+
+      def initialize
+        @base_path = '.'
+        @logo = 'logo.png'
+        @logo_2x = 'logo@2x.png'
+        @icon = 'icon.png'
+        @icon_2x = 'icon@2x.png'
+      end
     end
   end
 end
---
+++ spec/lib/passbook/configuration_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe Hoarder::Passbook::Configuration do
+  subject { Hoarder::Passbook::Configuration.new }
+  describe ".initialize" do
+    it "defaults base_path to ." do
+      subject.base_path.should == '.'
+    end
+
+    it "defaults logo to logo.png" do
+      subject.logo.should == 'logo.png'
+    end
+
+    it "defaults logo_2x to logo@2x.png" do
+      subject.logo_2x.should == 'logo@2x.png'
+    end
+
+    it "defaults icon to icon.png" do
+      subject.icon.should == 'icon.png'
+    end
+
+    it "defaults icon_2x to icon@2x.png" do
+      subject.icon_2x.should == 'icon@2x.png'
+    end
+  end
+end
