ee1c1177ff39527aea9294847bab9ca9829c570c
Jamie Wright <jamie@brilliantfantastic.com>
2012-12-02 18:00:50 -0500

    Implemented archive on pass

--- lib/hoarder/passbook/pass.rb
+++ lib/hoarder/passbook/pass.rb
@@ -1,7 +1,14 @@
+require 'ostruct'
+
 module Hoarder
   module Passbook
     class Pass
       attr_accessor :name
+
+      def archive
+        OpenStruct.new name:    "pass.json",
+                       stream:  self.to_json
+      end
 
       def to_json
       end
--- spec/lib/passbook/pass_spec.rb
+++ spec/lib/passbook/pass_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Hoarder::Passbook::Pass do
   subject { Hoarder::Passbook::Pass.new }
+  describe ".archive" do
+    it "sets the name to pass.json" do
+      subject.archive.name.should == "pass.json"
+    end
+
+    it "sets the stream to the json" do
+      subject.stub(:to_json => "hello: world" )
+      subject.archive.stream.should == subject.to_json
+    end
+  end
 end
